2a772b923d812b7c412d4ec3b7539d5fa7346038
Landon Abney <Landonabney@gmail.com>
2016-10-26 11:21:04 -0700

    Fix lint issues
    Biggest change is some refactoring of extractRange().

--- spec/linter-swagger-spec.js
+++ spec/linter-swagger-spec.js
@@ -1,93 +1,86 @@
 'use babel';
 
 import * as path from 'path';
+import { provideLinter } from '../lib/main';
+
+const lint = provideLinter().lint;
+
+const petstoreJSONPath = path.join(__dirname, 'fixtures', 'petstore.json');
+const petstoreYAMLPath = path.join(__dirname, 'fixtures', 'petstore.yaml');
+const petstoreBadref = path.join(__dirname, 'fixtures', 'petstore-badref.yaml');
 
 describe('The Swagger provider for Linter', () => {
-  const lint = require(path.join('..', 'lib', 'main.js')).provideLinter().lint;
-
   describe('linting JSON files', () => {
-
     beforeEach(() => {
       atom.workspace.destroyActivePaneItem();
       waitsForPromise(() => {
         atom.packages.activatePackage('linter-swagger');
         return atom.packages.activatePackage('language-json').then(() =>
-          atom.workspace.open(path.join(__dirname, 'fixtures', 'petstore.json'))
+          atom.workspace.open(petstoreJSONPath)
         );
       });
     });
 
     it('finds nothing wrong with valid file', () => {
-      waitsForPromise(() => {
-        const goodFile = path.join(__dirname, 'fixtures', 'petstore.json');
-        return atom.workspace.open(goodFile).then(editor => {
-          return lint(editor).then(messages => {
-            expect(messages.length).toEqual(0);
-          });
-        });
-      });
+      waitsForPromise(() =>
+        atom.workspace.open(petstoreJSONPath).then(editor =>
+          lint(editor).then(messages =>
+            expect(messages.length).toEqual(0)
+          )
+        )
+      );
     });
-
   });
 
   describe('linting YAML files', () => {
-
     beforeEach(() => {
       atom.workspace.destroyActivePaneItem();
       waitsForPromise(() => {
         atom.packages.activatePackage('linter-swagger');
         return atom.packages.activatePackage('language-yaml').then(() =>
-          atom.workspace.open(path.join(__dirname, 'fixtures', 'petstore.yaml'))
+          atom.workspace.open(petstoreYAMLPath)
         );
       });
     });
 
     describe('checks a file with issues', () => {
       let editor = null;
-      const badFile = path.join(__dirname, 'fixtures', 'petstore-badref.yaml');
 
       beforeEach(() => {
-        waitsForPromise(() => {
-          return atom.workspace.open(badFile).then(openEditor => {
+        waitsForPromise(() =>
+          atom.workspace.open(petstoreBadref).then((openEditor) => {
             editor = openEditor;
-          });
-        });
+          })
+        );
       });
 
-      it('finds at least one message', () => {
-        waitsForPromise(() => {
-          return lint(editor).then(messages => {
-            expect(messages.length).toBeGreaterThan(0);
-          });
-        });
-      });
+      it('finds at least one message', () =>
+        waitsForPromise(() =>
+          lint(editor).then(messages =>
+            expect(messages.length).toBeGreaterThan(0)
+          )
+        )
+      );
 
-      it('verifies the message', () => {
-        waitsForPromise(() => {
-          return lint(editor).then(messages => {
-            console.log(messages);
-            expect(messages[0].type).toBeDefined();
-            expect(messages[0].type).toEqual('Error');
+      it('verifies the message', () =>
+        waitsForPromise(() =>
+          lint(editor).then((messages) => {
+            expect(messages[0].type).toBe('Error');
             expect(messages[0].text).toBeDefined();
-            expect(messages[0].filePath).toBeDefined();
-            expect(messages[0].filePath).toMatch(/.+petstore\-badref\.yaml$/);
-          });
-        });
-      });
+            expect(messages[0].filePath).toBe(petstoreBadref);
+          })
+        )
+      );
     });
 
-    it('finds nothing wrong with valid file', () => {
-      waitsForPromise(() => {
-        const goodFile = path.join(__dirname, 'fixtures', 'petstore.yaml');
-        return atom.workspace.open(goodFile).then(editor => {
-          return lint(editor).then(messages => {
-            expect(messages.length).toEqual(0);
-          });
-        });
-      });
-    });
-
+    it('finds nothing wrong with a valid file', () =>
+      waitsForPromise(() =>
+        atom.workspace.open(petstoreYAMLPath).then(editor =>
+          lint(editor).then((messages) => {
+            expect(messages.length).toBe(0);
+          })
+        )
+      )
+    );
   });
-
-
 });
