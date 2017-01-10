016008d5de397ca25a123a0a68c39a48df5c2f80
Tony Fendall <github@tonyfendall.com>
2017-01-10 14:35:33 +1300

    Remove lodash dependency (where possible)
    Updated error ranges to target the document element `key` where possible (more useful)

--- spec/json-files-spec.js
+++ spec/json-files-spec.js
@@ -39,14 +39,14 @@ describe('Linting JSON files', () => {
           type: 'Error',
           text: 'No enum match for: strin',
           filePath: SAMPLE1,
-          range: [[19, 48], [19, 55]],
+          range: [[19, 40], [19, 46]],
         });
 
         expect(messages[1]).toEqual({
           type: 'Error',
           text: 'Expected type array but found type string',
           filePath: SAMPLE1,
-          range: [[19, 48], [19, 55]],
+          range: [[19, 40], [19, 46]],
         });
       });
     }));
@@ -64,7 +64,7 @@ describe('Linting JSON files', () => {
           type: 'Error',
           text: 'Additional properties not allowed: forma',
           filePath: SAMPLE2,
-          range: [[20, 49], [20, 55]],
+          range: [[20, 40], [20, 47]],
         });
       });
     }));
@@ -82,14 +82,14 @@ describe('Linting JSON files', () => {
           type: 'Error',
           text: 'Additional properties not allowed: anyOf',
           filePath: SAMPLE3,
-          range: [[13, 37], [38, 29]],
+          range: [[13, 28], [13, 35]],
         });
 
         expect(messages[1]).toEqual({
           type: 'Error',
           text: 'Missing required property: type',
           filePath: SAMPLE3,
-          range: [[13, 37], [38, 29]],
+          range: [[13, 28], [13, 35]],
         });
       });
     }));
@@ -108,6 +108,41 @@ describe('Linting JSON files', () => {
           text: `Error resolving $ref pointer "${SAMPLE4}#/definitions/INVALIDREFERENCE". \nToken "definitions" does not exist.`,
           filePath: SAMPLE4,
           range: undefined,
+        });
+      });
+    }));
+  });
+
+  it('Handles errors within an array', () => {
+    const SAMPLE5 = join(__dirname, 'fixtures', 'sample5.json');
+
+    waitsForPromise(() => atom.workspace.open(SAMPLE5).then((editor) => {
+      lint(editor).then((messages) => {
+        expect(messages.length).toEqual(1);
+
+        expect(messages[0]).toEqual({
+          type: 'Error',
+          text: 'Additional properties not allowed: descriptio',
+          filePath: SAMPLE5,
+          range: [[13, 6], [13, 18]],
+        });
+      });
+    }));
+  });
+
+
+  it('Handles nested properties with the same name', () => {
+    const SAMPLE6 = join(__dirname, 'fixtures', 'sample6.json');
+
+    waitsForPromise(() => atom.workspace.open(SAMPLE6).then((editor) => {
+      lint(editor).then((messages) => {
+        expect(messages.length).toEqual(2);
+
+        expect(messages[0]).toEqual({
+          type: 'Error',
+          text: 'No enum match for: strin',
+          filePath: SAMPLE6,
+          range: [[19, 20], [19, 26]],
         });
       });
     }));
